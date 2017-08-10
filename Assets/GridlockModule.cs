9d711682c35d342fd9f193920ba181f80701acb8
Timwi <github@timwi.de>
2017-08-10 20:40:28 +0200

    First commit

---
+++ Assets/GridlockModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gridlock;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Gridlock
+/// Created by Elias5891, implemented by Timwi
+/// </summary>
+public class GridlockModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
